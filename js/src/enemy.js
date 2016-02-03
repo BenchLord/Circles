9c8664a31b69ecf439c48a34ab04a2c1022797ac
Brandon Bench <BenchLord@Alveyworld-32.local>
2016-02-03 11:08:24 -0700

    minor spawn fixes

--- js/src/enemy.js
+++ js/src/enemy.js
@@ -1,34 +1,48 @@
 var Enemy = Class.extend({
   init: function(canvas, r, colors) {
-    this.r = Math.floor(Math.random() * (r + 20)) + (r - 5);
+    this.r = Math.floor(Math.random() * (r + 10)) + (r - 5);
     this.color = colors[Math.floor(Math.random() * (colors.length - 1))];
     
     var self = this;
     switch (Math.floor(Math.random() * 4) + 1) {
       case 1: // Top
         self.x = Math.floor(Math.random() * canvas.width);
-        self.y = 0;
+        if (self.r < 80) {
+          self.y = 0 - self.r;
+        } else {
+          self.y = 0;
+        }
         self.yVel = 2 - ((1/5) * Math.sqrt(self.r));
         var theta = (Math.random() * Math.PI);
         self.xVel = Math.cos(theta) * self.yVel;
       break;
       case 2: // Bottom
         self.x = Math.floor(Math.random() * canvas.width);
-        self.y = canvas.height;
+        if (self.r < 80) {
+          self.y = canvas.height + self.r;
+        }
         self.yVel = -2 + ((1/5) * Math.sqrt(self.r));
         var theta = (Math.random() * (2 * Math.PI)) + Math.PI;
         self.xVel = Math.sin(theta) * self.yVel;
       break;
       case 3: // Right
-        self.y = Math.floor(Math.random() * canvas.height);      
-        self.x = canvas.width;
+        self.y = Math.floor(Math.random() * canvas.height);
+        if (self.r < 80) {
+          self.x = canvas.width + self.r;
+        } else {
+          self.x = canvas.width;        
+        }     
         self.xVel = -2 + ((1/5) * Math.sqrt(self.r));
         var theta = (Math.random() * (2 * Math.PI)) + Math.PI;
         self.yVel = Math.sin(theta) * self.xVel;
       break;
       case 4: // Left
-        self.y = Math.floor(Math.random() * canvas.height);      
-        self.x = 0;
+        self.y = Math.floor(Math.random() * canvas.height); 
+        if (self.r < 80) {
+          self.x = 0 - self.r;
+        } else {
+          self.x = 0;
+        }
         self.xVel = 2 - ((1/5) * Math.sqrt(self.r));
         var theta = (Math.random() * Math.PI);
         self.yVel = Math.cos(theta) * self.xVel;
